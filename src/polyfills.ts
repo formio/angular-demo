4c61a2e732215274d3a4aa29163129d09fe5616d
Travis Tidwell <travis@form.io>
2019-08-28 10:53:32 -0500

    Updated 4.x build.

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -73,9 +73,6 @@
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
-
-
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-(window as any).global = window;

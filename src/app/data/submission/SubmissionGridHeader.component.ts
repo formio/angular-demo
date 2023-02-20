ec0fbabad268c35c713efa85baf5a806a6c29d4f
Hanna Kurban <Hanna.Kurban@itechart-group.com>
2023-02-20 15:55:52 +0300

    FIO-5549 Custom submission grid header

---
+++ src/app/data/submission/SubmissionGridHeader.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+import { SubmissionGridHeaderComponent } from "@formio/angular/grid";
+
+@Component({
+    templateUrl: './SubmissionGridHeader.component.html'
+})
+export class CustomSubmissionGridHeaderComponent extends SubmissionGridHeaderComponent {}
